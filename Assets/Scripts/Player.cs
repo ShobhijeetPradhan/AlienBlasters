0dbf15a1f18402598df6d2579c76b4d6f6255bdb
Shobhijeet Pradhan <shobhijeetpradhan@gmail.com>
2026-09-12 04:00:09 +0530

    Player Jump Scripting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,22 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float _jumpEndTime;
+    [SerializeField] private float _jumpVelocity = 5;
+    [SerializeField] private float _jumpDuration = 0.5f;
+    public bool IsGrounded;
+
+    void OnDrawGizmos()
     {
-        
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - spriteRenderer.bounds.extents.y);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - spriteRenderer.bounds.extents.y);
+        var hit = Physics2D.Raycast(origin, Vector2.down, 0.1f);
+        if (hit.collider)
+        {
+            IsGrounded = true;
+        }
+        else
+        {
+            IsGrounded = false;
+        }
+
         var horizontal = Input.GetAxis("Horizontal");
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         var vertical = rb.linearVelocity.y;
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.linearVelocity.y) < 0.001f)
+        if (Input.GetButtonDown("Jump") && IsGrounded)
         {
-            vertical = 5;
+            _jumpEndTime = Time.time + _jumpDuration;
+        }
+
+        if (Input.GetButton("Jump") && _jumpEndTime > Time.time)
+        {
+            vertical = _jumpVelocity;
         }
         rb.linearVelocity = new Vector2(horizontal, vertical);
     }
